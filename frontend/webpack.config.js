732502d88d0a5003de25a8e4c614d2f95bd31ded
Jess <sbtly.jess@gmail.com>
2020-05-01 22:15:48 +0900

    Revert "failed webpack"
    This reverts commit 3364c63b2a135458dac49958be6a0b9b38da3a6f.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,42 +1,13 @@
-// module.exports = {
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: "babel-loader",
-//         },
-//       },
-//     ],
-//   },
-// };
-
-var path = require("path");
-var webpack = require("webpack");
-var BundleTracker = require("webpack-bundle-tracker");
-
 module.exports = {
-  context: __dirname,
-
-  entry: "./src/index.js",
-
-  output: {
-    path: path.resolve("./static/frontend/main.js"),
-    // filename: "[name]-[hash].js",
-  },
-
-  plugins: [new BundleTracker({ filename: "./webpack-stats.json" })],
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: {
+          loader: "babel-loader",
+        },
       },
     ],
-  },
-  resolve: {
-    extensions: ["*", ".js", ".jsx"],
   },
 };
