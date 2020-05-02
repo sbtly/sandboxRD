b380fc8af875f7ca0a33236c7719f973a58b6aff
Jess <sbtly.jess@gmail.com>
2020-05-02 17:04:14 +0900

    failed

--- sandbox/frontend/src/UsersList.js
+++ sandbox/frontend/src/UsersList.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState, useCallback } from "react";
 import UserAPI from "./UserAPI";
+import ReactDataGrid from "react-data-grid";
 
 const userAPI = new UserAPI();
 
 const UsersList = () => {
   const [users, setUsers] = useState([]);
   const [nextPageURL, setNextPageURL] = useState("");
+
+  //   const [rows, setRows] = useState([]);
 
   const nextPage = useCallback(() => {
     userAPI.getUsersByURL(nextPageURL).then((result) => {
@@ -26,6 +29,31 @@ const UsersList = () => {
     [users]
   );
 
+  //   const columns = [
+  //     { key: "id", name: "ID" },
+  //     { key: "first_name", name: "First Name" },
+  //     { key: "last_name", name: "Last Name" },
+  //   ];
+
+  //   const rows = [
+  //     users.map((u) => ({
+  //       id: u.pk,
+  //       first_name: u.first_name,
+  //       last_name: u.last_name,
+  //     })),
+  //   ];
+  const columns = [
+    { key: "id", name: "ID" },
+    { key: "title", name: "Title" },
+  ];
+
+  const rows = [
+    { id: 0, title: "Example" },
+    { id: 1, title: "Demo" },
+  ];
+
+  console.log(rows);
+
   useEffect(() => {
     userAPI.getUsers().then(function (result) {
       setUsers(result.data);
@@ -35,7 +63,7 @@ const UsersList = () => {
 
   return (
     <div className="usersList">
-      <table className="table">
+      {/* <table className="table">
         <thead key="thead">
           <tr>
             <th>#</th>
@@ -54,10 +82,6 @@ const UsersList = () => {
               <td>{u.pk}</td>
               <td>{u.first_name}</td>
               <td>{u.last_name}</td>
-              {/* <td>{u.phone}</td>
-              <td>{u.email}</td>
-              <td>{u.address}</td>
-              <td>{u.description}</td> */}
               <td>
                 <button onClick={(e) => handleDelete(e, u.pk)}>Delete</button>
                 <a href={"/user/" + u.pk}>Update</a>
@@ -65,7 +89,15 @@ const UsersList = () => {
             </tr>
           ))}
         </tbody>
-      </table>
+      </table> */}
+
+      <ReactDataGrid
+        columns={columns}
+        rowGetter={(i) => rows[i]}
+        rowsCount={3}
+        // minHeight={150}
+      ></ReactDataGrid>
+
       <button className="btn" onClick={nextPage}>
         Next
       </button>
