b882ec9bd0ec5c37a9c234ff6d8faec039bdc19f
Jess <sbtly.jess@gmail.com>
2020-05-02 18:17:43 +0900

    test completed

--- sandbox/frontend/src/UsersList.js
+++ sandbox/frontend/src/UsersList.js
@@ -27,38 +27,6 @@ const UsersList = () => {
     [users]
   );
 
-  //   const data = useMemo(
-  //     () => [
-  //       {
-  //         col1: "Hello",
-  //         col2: "World",
-  //       },
-  //       {
-  //         col1: "react-table",
-  //         col2: "rocks",
-  //       },
-  //       {
-  //         col1: "whatever",
-  //         col2: "you want",
-  //       },
-  //     ],
-  //     []
-  //   );
-
-  //   const columns = useMemo(
-  //     () => [
-  //       {
-  //         Header: "Column 1",
-  //         accessor: "col1", // accessor is the "key" in the data
-  //       },
-  //       {
-  //         Header: "Column 2",
-  //         accessor: "col2",
-  //       },
-  //     ],
-  //     []
-  //   );
-
   const data = useMemo(
     () =>
       users.map((u) => ({
@@ -69,16 +37,6 @@ const UsersList = () => {
     [users]
   );
 
-  //   const data = useMemo(
-  //     () => [
-  //       users.map((u) => ({
-  //         pk: u.pk,
-  //         first_name: u.first_name,
-  //         last_name: u.last_name,
-  //       })),
-  //     ],
-  //     []
-  //   );
   const columns = useMemo(
     () => [
       {
