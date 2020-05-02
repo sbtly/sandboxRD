1945b561d39d5cfcc1f493361f370b35e57fb79f
Jess <sbtly.jess@gmail.com>
2020-05-02 18:14:47 +0900

    success!!!! :innocent:

--- sandbox/frontend/src/UsersList.js
+++ sandbox/frontend/src/UsersList.js
@@ -27,19 +27,58 @@ const UsersList = () => {
     [users]
   );
 
+  //   const data = useMemo(
+  //     () => [
+  //       {
+  //         col1: "Hello",
+  //         col2: "World",
+  //       },
+  //       {
+  //         col1: "react-table",
+  //         col2: "rocks",
+  //       },
+  //       {
+  //         col1: "whatever",
+  //         col2: "you want",
+  //       },
+  //     ],
+  //     []
+  //   );
+
+  //   const columns = useMemo(
+  //     () => [
+  //       {
+  //         Header: "Column 1",
+  //         accessor: "col1", // accessor is the "key" in the data
+  //       },
+  //       {
+  //         Header: "Column 2",
+  //         accessor: "col2",
+  //       },
+  //     ],
+  //     []
+  //   );
+
   const data = useMemo(
-    () => [
-      users.map((u) => {
-        return {
-          pk: u.pk,
-          first_name: u.first_name,
-          last_name: u.last_name,
-        };
-      }),
-    ],
-    []
+    () =>
+      users.map((u) => ({
+        pk: u.pk,
+        first_name: u.first_name,
+        last_name: u.last_name,
+      })),
+    [users]
   );
 
+  //   const data = useMemo(
+  //     () => [
+  //       users.map((u) => ({
+  //         pk: u.pk,
+  //         first_name: u.first_name,
+  //         last_name: u.last_name,
+  //       })),
+  //     ],
+  //     []
+  //   );
   const columns = useMemo(
     () => [
       {
@@ -73,6 +112,7 @@ const UsersList = () => {
       setUsers(result.data);
       setNextPageURL(result.nextlink);
     });
+    console.log(data);
   }, []);
 
   return (
@@ -107,22 +147,26 @@ const UsersList = () => {
 
       <table {...getTableProps()}>
         <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+          {headerGroups.map((headerGroup, i) => (
+            <tr key={i} {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column, i) => (
+                <th key={i} {...column.getHeaderProps()}>
+                  {column.render("Header")}
+                </th>
               ))}
             </tr>
           ))}
         </thead>
         <tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
+          {rows.map((row, i) => {
             prepareRow(row);
             return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
+              <tr key={i} {...row.getRowProps()}>
+                {row.cells.map((cell, i) => {
                   return (
-                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    <td key={i} {...cell.getCellProps()}>
+                      {cell.render("Cell")}
+                    </td>
                   );
                 })}
               </tr>
