124ddda759ed3295b827fb29b0fafb7473ba8807
Jess <sbtly.jess@gmail.com>
2020-05-03 22:33:58 +0900

    got a sense of it - styled-component + react-spring + react-use-gesture :sunglasses:

--- sandbox/frontend/src/components/Sidebar.js
+++ sandbox/frontend/src/components/Sidebar.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { useSpring, animated } from "react-spring";
+import { useHover } from "react-use-gesture";
 
-const SidebarDiv = styled.div`
+const vars = {
+  left: -120,
+};
+
+const SidebarDiv = styled(animated.div)`
   position: fixed;
   border-radius: 0 16px 16px 0;
-
-  width: ${(props) => (props.expand ? 200 : 80)}px;
+  left: vars.left;
+  /* width: ${(props) => (props.expand ? 200 : 80)}px; */
   /* height: calc(100vh - 20px); */
+  width: 200px;
   height: 100%;
   background: #fff;
 `;
 
 const Sidebar = () => {
-  return <SidebarDiv></SidebarDiv>;
+  // const [expand, setExpand] = useState(false);
+  const [animate, set] = useSpring(() => ({ left: vars.left }));
+  const gesture = useHover(({ hovering }) =>
+    set({ left: hovering ? 0 : vars.left })
+  );
+
+  return <SidebarDiv {...gesture()} style={animate}></SidebarDiv>;
 };
 
 export default Sidebar;
